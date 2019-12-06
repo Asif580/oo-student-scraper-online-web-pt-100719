111b9333a2e4ae780fc90a34e267b7b358b786b1
Asiful Choudhury <asiful307@gmail.com>
2019-12-06 00:27:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    self.each do |index_url|
+      contribute & escape = 
   end
 
   def self.scrape_profile_page(profile_url)
